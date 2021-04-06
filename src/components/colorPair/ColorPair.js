6e298b76e1b6051086e7673debcb78a56d369476
Sean Turley <seanmturley@gmail.com>
2021-04-06 16:27:50 +0200

    Add color and heading to color pairs

--- src/components/colorPair/ColorPair.js
+++ src/components/colorPair/ColorPair.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function ColorPair({ hex, contrast, textSize }) {
   return (
-    <article>
+    <article style={{ color: `#${hex}` }}>
       <p>{Math.round((contrast + Number.EPSILON) * 10) / 10} : 1</p>
-      <p>{hex}</p>
-      <p>Favourite</p>
+      <h1>{hex}</h1>
+      {/* <p>Favourite</p> */}
       <p>{textSize} text</p>
     </article>
   );
