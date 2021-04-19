2501f8d754e51b3ee54fbac62e5982bd600eec52
Sean Turley <seanmturley@gmail.com>
2021-04-19 09:13:13 +0200

    Add styling to PaletteInput

--- src/components/paletteInput/PaletteInput.js
+++ src/components/paletteInput/PaletteInput.js
@@ -1,22 +1,34 @@
 import React from "react";
 
+import "./PaletteInput.css";
+
 function PaletteInput({ rawPalette, handleInputChange, handleInputSubmit }) {
   return (
-    <section>
+    <section className="palette-input">
       <form aria-label="form" onSubmit={handleInputSubmit}>
-        <label htmlFor="palette-input">Add palette</label>
+        <div className="palette-input__controls">
+          <label className="palette-input__heading" htmlFor="palette-input">
+            Add palette
+          </label>
+          <input
+            className="palette-input__submit-button"
+            type="submit"
+            value="+"
+          />
+        </div>
         <textarea
+          className="palette-input__text-area"
           type="textarea"
           name="palette-input"
           id="palette-input"
           aria-multiline="true"
           placeholder="Enter a color palette"
-          rows={15}
-          cols={45}
+          rows={12}
+          cols={36}
           value={rawPalette}
           onChange={handleInputChange}
+          spellCheck="false"
         />
-        <input type="submit" value="Submit" />
       </form>
     </section>
   );
