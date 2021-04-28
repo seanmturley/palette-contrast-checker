f8c4522648e162fa2aa4c7151fc49021adc347ce
Sean Turley <seanmturley@gmail.com>
2021-04-28 13:09:08 +0200

    Fix PropType errors, and add tests for text size styling and grayscale in ColorPair.test.js

--- src/components/colorPair/ColorPair.test.js
+++ src/components/colorPair/ColorPair.test.js
@@ -7,18 +7,39 @@ import { render, screen } from "@testing-library/react";
 const setup = (color) => {
   render(
     <ColorPair
-      hex={color.hex}
+      pairColor={color.pairColor}
+      pairGrayscaleEquivalent={color.pairGrayscaleEquivalent}
       contrast={color.contrast}
+      hoverGrowClass={color.hoverGrowClass}
       textSize={color.textSize}
+      hidePairs={color.hidePairs}
+      grayscale={color.grayscale}
     />
   );
 };
 
 const color = {
-  hex: "ffffff",
-  contrast: 21,
-  textSize: "Normal",
-  textRgb: "rgb(255, 255, 255)"
+  pairColor: "468faf",
+  pairGrayscaleEquivalent: "828282",
+  contrast: 3.6,
+  hoverGrowClass: "",
+  textSize: "Large",
+  hidePairs: "",
+  grayscale: false,
+  textRgb: "rgb(70, 143, 175)",
+  textGrayscaleRgb: "rgb(130, 130, 130)"
+};
+
+const colorGrayscaleMode = {
+  pairColor: "468faf",
+  pairGrayscaleEquivalent: "828282",
+  contrast: 3.6,
+  hoverGrowClass: "",
+  textSize: "Large",
+  hidePairs: "",
+  grayscale: true,
+  textRgb: "rgb(70, 143, 175)",
+  textGrayscaleRgb: "rgb(130, 130, 130)"
 };
 
 describe("Color pair", () => {
@@ -36,13 +57,29 @@ describe("Color pair", () => {
 
   it("should display the color's hex value", () => {
     setup(color);
-    const colorHexValue = screen.queryByText(`${color.hex}`);
+    const colorHexValue = screen.queryByText(`${color.pairColor}`);
     expect(colorHexValue).toBeInTheDocument();
   });
 
   it("should display the color pairing's appropriate text size", () => {
     setup(color);
-    const colorPairTextSize = screen.queryByText(`${color.textSize} text`);
+    const colorPairTextSize = screen.queryByText(`${color.textSize} font`);
     expect(colorPairTextSize).toBeInTheDocument();
+  });
+
+  it("should display the color pairings's text in bold if large text is required", () => {
+    setup(color);
+    const colorPairTextSize = screen.queryByText(`${color.textSize} font`);
+    expect(colorPairTextSize).toHaveClass(
+      `color-pair__text-size--${color.textSize}`
+    );
+  });
+
+  it("should have text color set to the color's grayscale equivalent rgb value", () => {
+    setup(colorGrayscaleMode);
+    const colorPair = screen.getByTestId("color-pair");
+    expect(colorPair).toHaveStyle(
+      `color: ${colorGrayscaleMode.textGrayscaleRgb}`
+    );
   });
 });
