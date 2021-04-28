dec95912576f44b9caaf538ddfe3600ccbdb1869
Sean Turley <seanmturley@gmail.com>
2021-04-28 14:03:16 +0200

    Fix PropType errors in PaletteDisplay.test.js

--- src/components/paletteDisplay/PaletteDisplay.test.js
+++ src/components/paletteDisplay/PaletteDisplay.test.js
@@ -4,18 +4,41 @@ import PaletteDisplay from "./PaletteDisplay";
 
 import { render, screen } from "@testing-library/react";
 
-const setup = (paletteData, allColorPairs) => {
-  render(
-    <PaletteDisplay paletteData={paletteData} allColorPairs={allColorPairs} />
-  );
+const setup = (palette) => {
+  render(<PaletteDisplay {...palette} />);
+};
+
+const emptyPalette = {
+  paletteData: [],
+  allColorPairs: {},
+  maxPairsCount: 0,
+  showPaletteInput: true,
+  contrastStandard: "aa",
+  grayscale: false,
+  theme: "both"
 };
 
 const mockPaletteData = [
-  { hex: "000000" },
-  { hex: "14213d" },
-  { hex: "fca311" },
-  { hex: "e5e5e5" },
-  { hex: "ffffff" }
+  { hex: "000000", grayscaleEquivalent: "000000", luminance: 0, theme: "dark" },
+  {
+    hex: "14213d",
+    grayscaleEquivalent: "202020",
+    luminance: 0.015733572954602534,
+    theme: "dark"
+  },
+  {
+    hex: "fca311",
+    grayscaleEquivalent: "ababab",
+    luminance: 0.4693030343862599,
+    theme: "light"
+  },
+  {
+    hex: "e5e5e5",
+    grayscaleEquivalent: "e5e5e5",
+    luminance: 0.7835377915261935,
+    theme: "light"
+  },
+  { hex: "ffffff", grayscaleEquivalent: "ffffff", luminance: 1, theme: "light" }
 ];
 
 const mockAllColorPairs = {
@@ -23,341 +46,231 @@ const mockAllColorPairs = {
     aa: [
       {
         hex: "ffffff",
+        grayscaleEquivalent: "ffffff",
         contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "e5e5e5",
+        grayscaleEquivalent: "e5e5e5",
         contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "fca311",
+        grayscaleEquivalent: "ababab",
         contrast: 10.4,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
     aaa: [
       {
         hex: "ffffff",
+        grayscaleEquivalent: "ffffff",
         contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "e5e5e5",
+        grayscaleEquivalent: "e5e5e5",
         contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "fca311",
+        grayscaleEquivalent: "ababab",
         contrast: 10.4,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
-    aaColorblind: [
-      {
-        hex: "ffffff",
-        contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "e5e5e5",
-        contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ],
-    aaaColorblind: [
-      {
-        hex: "ffffff",
-        contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "e5e5e5",
-        contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ]
+    aaPairsCount: 3,
+    aaaPairsCount: 3
   },
   "14213d": {
     aa: [
       {
         hex: "ffffff",
+        grayscaleEquivalent: "ffffff",
         contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "e5e5e5",
+        grayscaleEquivalent: "e5e5e5",
         contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "fca311",
+        grayscaleEquivalent: "ababab",
         contrast: 7.9,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
     aaa: [
       {
         hex: "ffffff",
+        grayscaleEquivalent: "ffffff",
         contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "e5e5e5",
+        grayscaleEquivalent: "e5e5e5",
         contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "fca311",
+        grayscaleEquivalent: "ababab",
         contrast: 7.9,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
-    aaColorblind: [
-      {
-        hex: "ffffff",
-        contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "e5e5e5",
-        contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ],
-    aaaColorblind: [
-      {
-        hex: "ffffff",
-        contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "e5e5e5",
-        contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ]
+    aaPairsCount: 3,
+    aaaPairsCount: 3
   },
   fca311: {
     aa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 10.4,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 7.9,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
     aaa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 10.4,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 7.9,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: false
+        aa: "Any",
+        aaa: "Any"
       }
     ],
-    aaColorblind: [],
-    aaaColorblind: []
+    aaPairsCount: 2,
+    aaaPairsCount: 2
   },
   e5e5e5: {
     aa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       }
     ],
     aaa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       }
     ],
-    aaColorblind: [
-      {
-        hex: "000000",
-        contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "14213d",
-        contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ],
-    aaaColorblind: [
-      {
-        hex: "000000",
-        contrast: 16.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "14213d",
-        contrast: 12.7,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ]
+    aaPairsCount: 2,
+    aaaPairsCount: 2
   },
   ffffff: {
     aa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       }
     ],
     aaa: [
       {
         hex: "000000",
+        grayscaleEquivalent: "000000",
         contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       },
       {
         hex: "14213d",
+        grayscaleEquivalent: "202020",
         contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
+        aa: "Any",
+        aaa: "Any"
       }
     ],
-    aaColorblind: [
-      {
-        hex: "000000",
-        contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "14213d",
-        contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ],
-    aaaColorblind: [
-      {
-        hex: "000000",
-        contrast: 21,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      },
-      {
-        hex: "14213d",
-        contrast: 16,
-        aa: "Normal",
-        aaa: "Normal",
-        colorblindSafe: true
-      }
-    ]
+    aaPairsCount: 2,
+    aaaPairsCount: 2
   }
+};
+
+const palette = {
+  paletteData: mockPaletteData,
+  allColorPairs: mockAllColorPairs,
+  maxPairsCount: 3,
+  showPaletteInput: false,
+  contrastStandard: "aa",
+  grayscale: false,
+  theme: "both"
 };
 
 describe("Palette display", () => {
   it("should render no ColorStripes when the paletteData is empty", () => {
-    setup([]);
+    setup(emptyPalette);
     const stripes = screen.queryAllByTestId("color-stripe");
     expect(stripes.length).toBe(0);
   });
 
   it("should render one ColorStripe for each color in paletteData", () => {
-    setup(mockPaletteData, mockAllColorPairs);
+    setup(palette);
     const stripes = screen.queryAllByTestId("color-stripe");
     expect(stripes.length).toBe(5);
   });
