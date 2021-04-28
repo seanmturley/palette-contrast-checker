283fdb2925f8d258c7bffd807e9923ad6b3967e1
Sean Turley <seanmturley@gmail.com>
2021-04-28 11:46:36 +0200

    Fix PropType errors and add grayscale test in ColorStripe.test.js

--- src/components/colorStripe/ColorStripe.test.js
+++ src/components/colorStripe/ColorStripe.test.js
@@ -8,23 +8,53 @@ const setup = (color) => {
   render(
     <ColorStripe
       stripeColor={color.hex}
-      stripeLabelColor={color.theme === "dark" ? "FFFFFF" : "000000"}
+      grayscaleEquivalent={color.grayscaleEquivalent}
+      hoverGrowClass={color.hoverGrowClass}
+      hidePairs={color.hidePairs}
+      contrastStandard={color.contrastStandard}
+      grayscale={color.grayscale}
+      stripeTheme={color.stripeTheme}
     />
   );
 };
 
 const darkColor = {
   hex: "14213d",
-  theme: "dark",
+  grayscaleEquivalent: "202020",
+  hoverGrowClass: "",
+  hidePairs: "",
+  contrastStandard: "aa",
+  grayscale: false,
+  stripeTheme: "dark",
   backgroundRgb: "rgb(20, 33, 61)",
+  grayscaleRgb: "rgb(32, 32, 32)",
   textRgb: "rgb(255, 255, 255)"
 };
 
 const lightColor = {
   hex: "e5e5e5",
-  theme: "light",
+  grayscaleEquivalent: "e5e5e5",
+  hoverGrowClass: "",
+  hidePairs: "",
+  contrastStandard: "aa",
+  grayscale: false,
+  stripeTheme: "light",
   backgroundRgb: "rgb(229, 229, 229)",
+  grayscaleRgb: "rgb(229, 229, 229)",
   textRgb: "rgb(0, 0, 0)"
+};
+
+const darkColorGrayscaleMode = {
+  hex: "14213d",
+  grayscaleEquivalent: "202020",
+  hoverGrowClass: "",
+  hidePairs: "",
+  contrastStandard: "aa",
+  grayscale: true,
+  stripeTheme: "dark",
+  backgroundRgb: "rgb(20, 33, 61)",
+  grayscaleRgb: "rgb(32, 32, 32)",
+  textRgb: "rgb(255, 255, 255)"
 };
 
 describe("Color stripe", () => {
@@ -43,12 +73,20 @@ describe("Color stripe", () => {
   it("should display the color's hex value in white if the color is dark", () => {
     setup(darkColor);
     const stripeLabel = screen.getByText(darkColor.hex);
-    expect(stripeLabel).toHaveStyle(`color: ${darkColor.textRgb}`);
+    expect(stripeLabel).toHaveClass("color-stripe__heading--theme-dark");
   });
 
   it("should display the color's hex value in black if the color is light", () => {
     setup(lightColor);
     const stripeLabel = screen.getByText(lightColor.hex);
-    expect(stripeLabel).toHaveStyle(`color: ${lightColor.textRgb}`);
+    expect(stripeLabel).toHaveClass("color-stripe__heading--theme-light");
+  });
+
+  it("should have a background-color equal to the color's grayscale equivalent rgb value in grayscale mode", () => {
+    setup(darkColorGrayscaleMode);
+    const stripe = screen.getByTestId("color-stripe");
+    expect(stripe).toHaveStyle(
+      `background-color: ${darkColorGrayscaleMode.grayscaleRgb}`
+    );
   });
 });
