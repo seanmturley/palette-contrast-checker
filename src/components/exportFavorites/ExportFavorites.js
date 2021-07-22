f5e6b7e4e734f1f80dfdd87277e953a6f945ae03
Sean Turley <seanmturley@gmail.com>
2021-07-22 08:49:00 +0200

    Style export modal close button and code field

--- src/components/exportFavorites/ExportFavorites.js
+++ src/components/exportFavorites/ExportFavorites.js
@@ -9,15 +9,16 @@ function ExportFavorites() {
     <section className="export-favorites" data-testid="export-favorites">
       <section className="export-favorites__top-controls">
         <h1 className="export-favorites__heading">Export favorites</h1>
-        <button className="export-favorites__close-button">x</button>
+        <button className="export-favorites__close-button">+</button>
       </section>
       <section className="export-favorites__code-container">
         <code className="export-favorites__code">
           {`html {
           /* WCAG minimum for low-contrast pairings: */
-          --large-font-required: 18pt; /* or 14pt & bold */
+          --large-font-required: 18pt; /* or 14pt bold */
 
-          /* High-contrast pairings can use any reasonable font size e.g.: */
+          /* High-contrast pairings can use any reasonable
+          font size e.g.: */
           --any-font-size: 12pt;
         }
 
